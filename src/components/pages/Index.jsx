9d1bcbdfaa6bcf7b8ea3eaeccf52a7bf47bbabc9
herrenojuliancoltrade <herrenojulian@coltrade.com.co>
2025-11-06 15:46:16 -0500

    first commit

---
+++ src/components/pages/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Bienvenido a la aplicación</h1>
+      <p>¡Por un nuevo futuro!</p>
+      <a href="/iniciar-sesion">Holi</a>
+    </div>
+  );
+};
+
+export default Index;
